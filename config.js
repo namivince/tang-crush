e454bbca3d86bbbbb98ac5d3f1cec3a14f0de48a
Vinceyen <159403781+namivince@users.noreply.github.com>
2024-02-08 23:22:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Dị nào công chúa rảnh anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'OkiiIii lun <3',
     messLink: 'https://www.facebook.com/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
